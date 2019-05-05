63e842c13232703d9528e06fa2365cb9c173177e
Hui An Yang <hecatie36@hotmail.com>
2019-05-05 22:25:01 +0100

    fix test conflects

--- test/bst-test.js
+++ test/bst-test.js
@@ -27,65 +27,12 @@ const alexa = va.VirtualAlexa.Builder()
 
       it('say help can listen to rules', async () => {
         let result = await alexa.launch();
-<<<<<<< HEAD
-        assert.include(result.prompt(), 'Welcome to Quizza');
-        result = await alexa.utter('start game');
-        assert.include(result.prompt(), 'A,');
-      })
-
-      it("Answer question incorrectly", async () => {
-        let result = await alexa.launch();
-        assert.include(result.prompt(), 'Welcome to Quizza');
-
-        result = await alexa.utter('start game');
-        assert.include(result.prompt(), 'A,');
-        result = await alexa.utter("the answer is b")
-        assert.include(result.prompt(), "sorry, that is wrong");
-=======
         result = await alexa.utter('help');
         assert.include(result.prompt(), 'Here are the rules.');
->>>>>>> temp_branch
       })
       
       it("Shows your score after 5 questions", async () => {
         let result = await alexa.launch();
-<<<<<<< HEAD
-        assert.include(result.prompt(), 'Welcome to Quizza');
-        result = await alexa.utter('start game');
-        assert.include(result.prompt(), 'A,');
-        console.log('1st')
-        console.log(result)
-        result = await alexa.utter("the answer is b")
-        assert.include(result.prompt(), "sorry, that is wrong");
-        console.log('2nd')
-        console.log(result)
-        result = await alexa.utter("the answer is c")
-        assert.include(result.prompt(), "sorry, that is wrong");
-        console.log('3rd')
-        console.log(result)
-        result = await alexa.utter("the answer is c")
-        assert.include(result.prompt(), "sorry, that is wrong");
-        console.log('4th')
-        console.log(result)
-        result = await alexa.utter("the answer is b")
-        assert.include(result.prompt(), "You scored 0");
-      })
-
-      it("Answer question correctly", async () => {
-        let result = await alexa.launch();
-        assert.include(result.prompt(), 'Welcome to Quizza');
-        result = await alexa.utter('start game');
-        assert.include(result.prompt(), 'A,');
-        result = await alexa.utter("the answer is a")
-        assert.include(result.prompt(), "correct");
-      })
-
-      it('will handle unrecognized input', async () => {
-        let result = await alexa.launch();
-        assert.include(result.prompt(), 'Welcome to Quizza');
-        result = await alexa.utter('chicken');
-        assert.include(result.prompt(), 'I do not understand that command');
-=======
         result = await alexa.utter('start game');
         assert.include(result.prompt(), 'This is round 1');
         result = await alexa.utter("the answer is a")
@@ -98,7 +45,6 @@ const alexa = va.VirtualAlexa.Builder()
         assert.include(result.prompt(), "Thank you for playing");
         // result = await alexa.utter("cheese")
         // assert.include(result.prompt(), "I am having trouble understanding that command");
->>>>>>> temp_branch
       })
       it('StopIntent', async () => {
         let result = await alexa.launch();
