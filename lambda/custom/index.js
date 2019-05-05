5a4d1f1cf9aa842c26b86de011fc26a378f38c58
Hui An Yang <hecatie36@hotmail.com>
2019-05-05 22:20:05 +0100

    refactored to update naming conventions

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,43 +2,44 @@ const Alexa = require('ask-sdk-core')
 const questions = require('./questions')
 const {welcomeMessage, goodbyeMessage, readyMessage, readyReprompt, helpMessage} = require('./messages')
 const questionNoPerRound = 2
+const totalRounds = 2
 // HANDLERS
 const LaunchRequestHandler = {
- canHandle(handlerInput) {
+  canHandle(handlerInput) {
   return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
-  || handlerInput.requestEnvelope.request.type === 'IntentRequest'
-  && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
- },
- handle(handlerInput) {
-  return handlerInput.responseBuilder
-  .speak(`<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_intro_01'/> ${welcomeMessage}`)
-  .withShouldEndSession (false)
-  .getResponse()
- }
+    || handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StartOverIntent'
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+    .speak(`<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_intro_01'/> ${welcomeMessage}`)
+    .withShouldEndSession (false)
+    .getResponse()
+  }
 }
 
 const ReadyIntentHandler = {
- canHandle(handlerInput) {
-  return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-   && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent'
- },
- handle(handlerInput) {
-  const slots = handlerInput.requestEnvelope.request.intent.slots
-  const yesAnswer = slots['yes'].value
-  const noAnswer = slots['no'].value
-  var speechText = ''
- if (yesAnswer === 'yes') {
-  speechText = `${readyMessage} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_waiting_loop_30s_01'/>`
-  } 
-  else if (noAnswer === 'no') {
-   speechText ='Well I don\'t have all day. Hurry up. <audio src=\'soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01\'/>'
-  }
-  return handlerInput.responseBuilder
-   .speak(speechText)
-   .reprompt(readyReprompt)
-   .withShouldEndSession (false)
-   .getResponse()
- },
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+    && handlerInput.requestEnvelope.request.intent.name === 'ReadyIntent'
+  },
+  handle(handlerInput) {
+    const slots = handlerInput.requestEnvelope.request.intent.slots
+    const yesAnswer = slots['yes'].value
+    const noAnswer = slots['no'].value
+    let speechText = ''
+    if (yesAnswer === 'yes') {
+      speechText = `${readyMessage} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_waiting_loop_30s_01'/>`
+    } 
+    else if (noAnswer === 'no') {
+      speechText ='Well I don\'t have all day. Hurry up. <audio src=\'soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01\'/>'
+    }
+    return handlerInput.responseBuilder
+    .speak(speechText)
+    .reprompt(readyReprompt)
+    .withShouldEndSession (false)
+    .getResponse()
+  },
 }
 
 const StartQuizIntentHandler = {
@@ -48,11 +49,11 @@ const StartQuizIntentHandler = {
  },
  handle(handlerInput) {
   let sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
-  sessionAttributes.total_rounds = 2
-  sessionAttributes.current_round = 1
+  sessionAttributes.totalRounds = totalRounds
+  sessionAttributes.currentRound = 1
   sessionAttributes.score = 0 
-  sessionAttributes.round1Questions = choose_questions(questions.ROUND1QUESTION)
-  sessionAttributes.round2Questions = choose_questions(questions.ROUND2QUESTION)
+  sessionAttributes.round1Questions = chooseQuestions(questions.ROUND1QUESTION)
+  sessionAttributes.round2Questions = chooseQuestions(questions.ROUND2QUESTION)
   let currentQuestionObject = sessionAttributes.round1Questions.pop()
   sessionAttributes.question = currentQuestionObject.question 
   sessionAttributes.answer = currentQuestionObject.answer
@@ -65,7 +66,7 @@ const StartQuizIntentHandler = {
   // TESTING ENDS
 
   return handlerInput.responseBuilder
-   .speak(`This is round ${sessionAttributes.current_round}. ${sessionAttributes.question} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01'/>`)
+   .speak(`This is round ${sessionAttributes.currentRound}. ${sessionAttributes.question} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01'/>`)
    .reprompt(sessionAttributes.question)
    .withShouldEndSession (false)
    .getResponse()
@@ -83,7 +84,7 @@ const AnswerIntentHandler = {
   const answerSlot = slot['answer'].value
 
   let speechText = ''
-  speechText+= determine_correct(answerSlot, sessionAttributes.answer, handlerInput)
+  speechText+= determineCorrect(answerSlot, sessionAttributes.answer, handlerInput)
   speechText+= getNextQuestion(handlerInput)
   return handlerInput.responseBuilder
    .speak(speechText)
@@ -134,14 +135,14 @@ const HelpIntentHandler = {
 
 // HELPER FUNCTIONS
 
-function determine_correct(answer_slot, session_attribute, handlerInput) {
+function determineCorrect(answerSlot, sessionAttribute, handlerInput) {
   const sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
   
-  if (answer_slot === session_attribute) {
+  if (answerSlot === sessionAttribute) {
     sessionAttributes.score ++
    return '<audio src=\'soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_tally_positive_01\'/> Correct. One point has been added to your score. '
   } 
-  else if (answer_slot === 'pass'|| answer_slot === 'skip' || answer_slot === 'I don\'t know') {
+  else if (answerSlot === 'pass'|| answerSlot === 'skip' || answerSlot === 'I don\'t know') {
    return '<audio src=\'soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_bridge_01\'/> Skipping that question.'
   } 
   else {
@@ -149,12 +150,12 @@ function determine_correct(answer_slot, session_attribute, handlerInput) {
   }
 }
 
-function choose_questions(questions_doc) {
-  var shuffled_array = shuffle_questions(questions_doc).slice(-questionNoPerRound)
-  return shuffled_array
+function chooseQuestions(questionsDoc) {
+  var shuffledArray = shuffleQuestions(questionsDoc).slice(-questionNoPerRound)
+  return shuffledArray
 }
 
-function shuffle_questions(array) {
+function shuffleQuestions(array) {
 
 	var currentIndex = array.length
 	var temporaryValue, randomIndex
@@ -171,8 +172,8 @@ function shuffle_questions(array) {
 
 function getNextQuestion(handlerInput) {
   const sessionAttributes = handlerInput.attributesManager.getSessionAttributes()
-  sessionAttributes.current_round === 1 && sessionAttributes.round1Questions.length === 0 ? sessionAttributes.current_round ++ : ''
-  if(sessionAttributes.current_round === 1){
+  sessionAttributes.currentRound === 1 && sessionAttributes.round1Questions.length === 0 ? sessionAttributes.currentRound ++ : ''
+  if(sessionAttributes.currentRound === 1){
       var currentQuestionObject = sessionAttributes.round1Questions.pop()
 
       var currentQuestion = currentQuestionObject.question 
@@ -190,7 +191,7 @@ function getNextQuestion(handlerInput) {
         return `The next question is: ${currentQuestion} <audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_countdown_loop_32s_full_01'/>`
 
     }
-    if(sessionAttributes.current_round === sessionAttributes.total_rounds){
+    if(sessionAttributes.currentRound === sessionAttributes.totalRounds){
       if(sessionAttributes.round2Questions.length === 0){
         return `You scored ${sessionAttributes.score}. Thank you for playing`
       }else if(sessionAttributes.round2Questions.length === questionNoPerRound){
